fdb17140ebb7ff078869832ec02c1040487e99a2
shivam7753 <shivamgupta.ece.21@nitap.ac.in>
2025-01-16 11:48:32 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+class  calculator {
+    static add(a,b){
+        return a+b;
+    }
+   static subtract(a,b){
+        return a-b;
+    }
+  static  multiply(a,b){
+        return a*b;
+    }
+   static divide(a,b){
+        return a/b;
+    }
+}
+ 
+console.log(calculator.add(2,3));
+console.log(calculator.subtract(5,2));
+console.log(calculator.multiply(3,4));
+console.log(calculator.divide(10,2));
+
